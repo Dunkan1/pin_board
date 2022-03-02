f2fda3087c41cef3ba7194f8f3adc323f5c1071e
Dunkan1 <dntorres1@uc.cl>
2022-03-02 16:00:20 -0300

    fix: multiple fixes in pin views html.haml

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,7 @@
 class PinsController < ApplicationController
+    before_action :find_pin, only: [:show, :edit, :update, :destroy]
     def index
+        @pins = Pin.all.order("created_at DESC")
     end
 
     def new
@@ -8,12 +10,34 @@ class PinsController < ApplicationController
 
     def create
         @pin = Pin.new(pin_params)
+
+        if @pin.save
+            redirect_to @pin, notice: "succesfully created new pin"
+        else
+            render 'new'
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy 
     end
 
     private
 
     def pin_params
         params.require(:pin).permit(:title, :description)
+    end
+
+    def find_pin
+        @pin = Pin.find(params[:id])
     end
 
 end
