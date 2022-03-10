7d1b4b8acc0eecae9af73a597f14d5562b78adff
Dunkan1 <dntorres1@uc.cl>
2022-03-10 11:25:06 -0300

    feat: add ajax to destroy button

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # resources :pins, to: 'pins#show', as: "pin_show"
+  resources :comments
   resources :pins do
     # post '/pins', to: 'pins#create', as: 'pin_creation'
     member do
       put "like", to: "pins#upvote"
       # put 'pinsasdf', to: 'piasdfns#create', as: 'my_new_article_in_member'
     end
+    resources :comments
   end
   
   post '/pins', to: 'pins#create', as: 'create_pin'
