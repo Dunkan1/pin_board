bebf2a91cee3870a74bf57b980800002f14b351a
Dunkan1 <dntorres1@uc.cl>
2022-03-11 10:25:32 -0300

    fix: model pin

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,11 +5,11 @@ class Pin < ActiveRecord::Base
     has_many :comments
 
     has_attached_file :image, styles: {medium: "300x300>"},
-    :s3_protocol => ‘https’,
-    :s3_host_name => ENV[‘S3_HOST_NAME’],
-    :path => ENV[‘S3_PATH’],
-    :storage => ‘s3’,
-    :s3_region => ENV[‘AWS_REGION’]
+    :s3_protocol => "https",
+    :s3_host_name => ENV["S3_HOST_NAME"],
+    :path => ENV["S3_PATH"],
+    :storage => "s3",
+    :s3_region => ENV["AWS_REGION"]
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
     
     validates :title, presence: true
